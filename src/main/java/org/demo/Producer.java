0df162cf6dd968c878ecc7391aff47ac8c2c7d3b
Sola Otudeko <sotudeko@sonatype.com>
2021-10-29 11:24:37 +0100

    1

--- src/main/java/org/demo/Producer.java
+++ src/main/java/org/demo/Producer.java
@@ -3,7 +3,7 @@ package org.demo;
 public class Producer {
 
   public String hello(){
-    return "Message from the Producer... Hello World! It's a new day";
+    return "Message from the Producer... Hello World! It's a new day indeed";
   }
 
 }
